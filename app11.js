2907140b90642759abccfdd2f98cc67f6eb10e55
Patrick Ly <patrickkimly@gmail.com>
2019-07-13 22:58:25 -0700

    lesson 12, creating elements

--- app11.js
+++ app11.js
@@ -21,5 +21,20 @@ const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
+    //console.log(value);
+
+    // create elements
+    const li =  document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    // add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    // append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
 });
